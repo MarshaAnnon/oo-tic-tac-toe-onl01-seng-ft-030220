0cd22d73e0a6c5df3543a02f94539617b9415eba
Marsha Annon <marshaannon@gmail.com>
2020-03-14 23:32:12 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -92,7 +92,7 @@ class TicTacToe
     end
     binding.pry
     if won?
-      puts "Congratulations #{winner}!"
+      puts "Congratulations #{won?}!"
     elsif draw?
       puts "Cat's Game!"
     end
