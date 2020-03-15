6f4d9e0d005eafc653bea50f228850f662eb0791
Marsha Annon <marshaannon@gmail.com>
2020-03-15 01:27:12 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,7 +1,7 @@
 require "pry"
 
 class TicTacToe
-  
+
 
   WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [6,4,2]]
 
@@ -96,3 +96,5 @@ class TicTacToe
     end
   end
 end
+
+game = TicTacToe.new
